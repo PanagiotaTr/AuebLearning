835536d3beb566086910b930eea041ac50b71eb5
george_sorogkas <georgesor1@hotmail.com>
2024-12-26 19:01:59 +0200

    Compelted category.html

--- Part 2 - JS & Handlebars & React/index.js
+++ Part 2 - JS & Handlebars & React/index.js
@@ -6,5 +6,10 @@ const app = express()
 app.use('/public', 
     express.static(__dirname + '/public'))
 
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
 
 let server = app.listen(8080)
